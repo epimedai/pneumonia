37aec13195da87ce06d3ee90cde23fb64a661827
martijho <martinhovin91@gmail.com>
2018-09-06 18:11:50 +0200

    small kaggle info change
    setup now reads kaggle.json which is gitignored and use info to connect to kaggle before downloads dataset

--- setup_dataset.py
+++ setup_dataset.py
@@ -12,12 +12,12 @@ from imgaug import augmenters as iaa
 from tqdm import tqdm
 import pandas as pd
 import glob
+import json
 
-from kaggle_info import KaggleInfoContainer
+kaggle_info = json.load(open('kaggle.json', 'r'))
 
-kaggle_info = KaggleInfoContainer()
-os.environ['KAGGLE_USERNAME'] = kaggle_info.kaggle_username
-os.environ['KAGGLE_KEY'] = kaggle_info.kaggle_key
+os.environ['KAGGLE_USERNAME'] = kaggle_info.username
+os.environ['KAGGLE_KEY'] = kaggle_info.key
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath('./lesson3-data')
@@ -27,15 +27,15 @@ MODEL_DIR = os.path.join(ROOT_DIR, 'logs')
 
 if not os.path.exists(ROOT_DIR):
     os.makedirs(ROOT_DIR)
-    os.chdir(ROOT_DIR)
+os.chdir(ROOT_DIR)
 
-    # If you are unable to download the competition dataset, check to see if you have
-    # accepted the user agreement on the competition website.
+# If you are unable to download the competition dataset, check to see if you have
+# accepted the user agreement on the competition website.
 
-    # Downloading and unziping dataset
-    os.system('kaggle competitions download -c rsna-pneumonia-detection-challenge')
+# Downloading and unziping dataset
+os.system('kaggle competitions download -c rsna-pneumonia-detection-challenge')
 
-    os.system('unzip -q -o stage_1_test_images.zip -d stage_1_test_images')
-    os.system('unzip -q -o stage_1_train_images.zip -d stage_1_train_images')
-    os.system('unzip -q -o stage_1_train_labels.csv.zip')
+os.system('unzip -q -o stage_1_test_images.zip -d stage_1_test_images')
+os.system('unzip -q -o stage_1_train_images.zip -d stage_1_train_images')
+os.system('unzip -q -o stage_1_train_labels.csv.zip')
 
